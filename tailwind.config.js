831cbca73d36deaba266ae307dbe99d55dbe8777
Shamal Khalid N P <shamalkhalidnp@gmail.com>
2024-03-13 15:25:06 +0530

    fix:default date selection

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,22 +2,21 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    colors: {
-      transparent: 'transparent',
-      current: 'currentColor',
-      'white': '#ffffff',
-      'purple': '#3f3cbb',
-      'midnight': '#121063',
-      'metal': '#565584',
-      'tahiti': '#3ab7bf',
-      'silver': '#ecebff',
-      'bubble-gum': '#ff77e9',
-      'bermuda': '#78dcca',
-      'greyish': '#606368'
-    },
     extend: {
       fontFamily:{
         sans: ["Open Sans"]
+      },
+      colors: {
+        transparent: 'transparent',
+        current: 'currentColor',
+        'white': '#ffffff',
+        'midnight': '#121063',
+        'metal': '#565584',
+        'tahiti': '#3ab7bf',
+        'silver': '#ecebff',
+        'bubble-gum': '#ff77e9',
+        'bermuda': '#78dcca',
+        'greyish': '#606368'
       },
       gridTemplateColumns: {
         "1/5" : "1fr 5fr"
